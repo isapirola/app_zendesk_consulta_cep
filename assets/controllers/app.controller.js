24612925227ceae03122feb02ffa6b36cc57792a
IsaPirola <belapirola@gmail.com>
2024-04-22 15:51:24 -0300

    Finish api request implementation

--- assets/controllers/app.controller.js
+++ assets/controllers/app.controller.js
@@ -1,8 +1,21 @@
-app.controller("app", [
+app.controller("AppController", [
     "$scope",
     "zendeskService",
     function ($scope, zendeskService) {
         var client = ZAFClient.init();
+
+        // Função para pesquisar dados do CEP
+        $scope.pesquisaCEP = function () {
+            zendeskService
+                .consultarCep($scope.cep)
+                .then(function (response) {
+                    // Armazenar os dados do CEP na variável $scope.cepData
+                    $scope.cepData = response.data;
+                })
+                .catch(function (error) {
+                    // Manipular erros aqui
+                    console.error(error);
+                });
+        };
     },
-    ($scope.pesquisaCEP = function () {}),
 ]);
