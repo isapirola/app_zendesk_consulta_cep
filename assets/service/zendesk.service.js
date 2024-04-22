519b8c0ede419f359f433db21b3bb469a6fbb544
IsaPirola <belapirola@gmail.com>
2024-04-22 15:03:56 -0300

    Create Zendesk Service

---
+++ assets/service/zendesk.service.js
@@ -0,0 +1,10 @@
+(function () {
+    "use strict";
+
+    app.service("zendeskService", function ($http) {
+        this.consultarCep = function (cep) {
+            var url = `https://viacep.com.br/ws/${cep}/json/`;
+            return $http.get(url);
+        };
+    });
+})();
