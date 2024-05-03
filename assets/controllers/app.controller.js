62aa526e85c0c12cb6818d0c47e17a7557b62585
IsaPirola <belapirola@gmail.com>
2024-05-03 14:58:20 -0300

    Handling errors with cep

--- assets/controllers/app.controller.js
+++ assets/controllers/app.controller.js
@@ -2,20 +2,33 @@ app.controller("AppController", [
     "$scope",
     "zendeskService",
     function ($scope, zendeskService) {
-        var client = ZAFClient.init();
-
         // Função para pesquisar dados do CEP
         $scope.pesquisaCEP = function () {
+            $scope.errorMessage = "";
+
             zendeskService
                 .consultarCep($scope.cep)
-                .then(function (response) {
-                    // Armazenar os dados do CEP na variável $scope.cepData
-                    $scope.cepData = response.data;
+                .then((response) => {
+                    if (response.erro) {
+                        // Se a resposta for um objeto vazio, exibir mensagem de erro
+                        $scope.cepData = {};
+                        $scope.errorMessage =
+                            "CEP não encontrado. Verifique se o CEP está correto e tente novamente.";
+                    } else {
+                        // Se a resposta não for vazia, exibir os dados do CEP
+                        $scope.cepData = response;
+                    }
                 })
                 .catch(function (error) {
-                    // Manipular erros aqui
+                    $scope.errorMessage = "CEP Inválido";
+                    $scope.cepData = {};
                     console.error(error);
                 });
+        };
+
+        // Função para verificar se um objeto está vazio
+        $scope.isEmptyObject = function (obj) {
+            return Object.keys(obj).length === 0 && obj.constructor === Object;
         };
     },
 ]);
--- assets/service/zendesk.service.js
+++ assets/service/zendesk.service.js
@@ -1,6 +1,21 @@
-app.service("zendeskService", function ($http) {
+app.service("zendeskService", function ($q) {
     this.consultarCep = function (cep) {
-        var url = `https://viacep.com.br/ws/${cep}/json/`;
-        return $http.get(url);
+        var deferred = $q.defer();
+        client
+            .request({
+                url: `https://viacep.com.br/ws/${cep}/json/`,
+                type: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then(function (response) {
+                deferred.resolve(response);
+            })
+            .catch(function (error) {
+                deferred.reject(error);
+            });
+
+        return deferred.promise;
     };
 });
